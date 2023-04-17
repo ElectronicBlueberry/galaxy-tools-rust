832fa002276f0e8aa53354283160c0e1025f13fd
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2023-04-17 09:47:56 +0200

    add unit tests for custom function

--- filter/src/functions.rs
+++ filter/src/functions.rs
@@ -14,3 +14,37 @@ pub fn create_function_context() -> HashMapContext {
 	}
 	.unwrap();
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use evalexpr::eval_boolean_with_context;
+
+	#[test]
+	fn in_function_on_tuple() {
+		let ctx = create_function_context();
+
+		assert!(eval_boolean_with_context(r#"in("a", ("a", "b", "c"))"#, &ctx).unwrap());
+		assert!(eval_boolean_with_context(r#"in(2, (1, 2, 3))"#, &ctx).unwrap());
+		assert!(eval_boolean_with_context(r#"in("foo", (1, 2, "foo", "bar"))"#, &ctx).unwrap());
+
+		assert_eq!(eval_boolean_with_context(r#"in("d", ("a", "b", "c"))"#, &ctx).unwrap(), false);
+		assert_eq!(eval_boolean_with_context(r#"in("2", (1, 2, 3))"#, &ctx).unwrap(), false);
+		assert_eq!(eval_boolean_with_context(r#"in(1, ("1", "2", "foo", "bar"))"#, &ctx).unwrap(), false);
+	}
+
+	#[test]
+	fn in_function_on_string() {
+		let ctx = create_function_context();
+
+		assert!(eval_boolean_with_context(r#"in("a", "abc")"#, &ctx).unwrap());
+		assert!(eval_boolean_with_context(r#"in("substring", "contains substring")"#, &ctx).unwrap());
+		assert!(eval_boolean_with_context(r#"in("bar", "foo bar baz")"#, &ctx).unwrap());
+
+		assert_eq!(eval_boolean_with_context(r#"in("d", "abc")"#, &ctx).unwrap(), false);
+		assert_eq!(eval_boolean_with_context(r#"in("2", "abc")"#, &ctx).unwrap(), false);
+		assert_eq!(eval_boolean_with_context(r#"in("contains substring", "substring")"#, &ctx).unwrap(), false);
+
+		assert!(eval_boolean_with_context(r#"in(1, "abc")"#, &ctx).is_err());
+	}
+}
